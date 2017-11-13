f5df6286435ff5cbe2cc3291dff5b7ab765ef7c8
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-13 00:08:46 -0200

    initial base files

---
+++ App.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+import { Movie } from './components/Movie'
+import { MovieList } from './components/MovieList'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev men',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MovieList></MovieList>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
