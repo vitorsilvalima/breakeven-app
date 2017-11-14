d5c91a21a256e4ba4aa34c6f0e35dc111ddc20c4
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-14 17:43:58 -0200

    refactor: changes Movie and MovieList components
    Made these components pure components

--- components/Movie.js
+++ components/Movie.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity, Text, View } from 'react-native';
 
 export const Movie = ({ onPress, movie }) => {
 
-    const { id, poster_path } = movie;
+    const { id, poster_path, title } = movie;
 
     return(
-        <TouchableOpacity onPress={() => onPress(id)}>
-            <Image 
-                source={{uri: `https://image.tmdb.org/t/p/w500/${poster_path}`}}
-                style={{width: 100, height: 150}}
-            />
-        </TouchableOpacity>
+        <View style={{width: 125, marginLeft: 2 }}>
+            <TouchableOpacity onPress={() => onPress(id)}>
+                <Text 
+                    style={{
+                        color: 'white', 
+                        textAlign: 'center',
+                        textAlignVertical: 'center',
+                        minHeight: 40
+                    }}
+                    ellipsizeMode={'tail'}
+                    numberOfLines={2}
+                >
+                    {title}
+                </Text>
+                <Image 
+                    source={{uri: `https://image.tmdb.org/t/p/w500/${poster_path}`}}
+                    style={{width: 125, height: 175}}
+                />
+                <View style={{width: 125, height: 40, flexDirection: 'row', flex: 1}}>
+                    <View style={{flexDirection: 'row', flex: 1, backgroundColor: 'white', height: 40}}>
+                        <Text>fdsfdsfdsfds</Text>
+                    </View>
+                    <View style={{flexDirection: 'row', flex: 1, backgroundColor: 'blue', height: 40}}>
+                        <Text>fdsfdsfds</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
