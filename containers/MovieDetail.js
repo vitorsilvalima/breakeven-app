77f8048020f7585270647b20d1638889aee38b95
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-22 22:48:08 -0200

    movieDetail: add fetch and some basic info

--- containers/MovieDetail.js
+++ containers/MovieDetail.js
@@ -1,11 +1,13 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
     View,
+    Image, 
+    Dimensions
 } from 'react-native'; 
 
-export class MovieDetail extends PureComponent{
+export class MovieDetail extends Component{
     static navigationOptions = {
         tabBarVisible: false,
         headerMode: 'screen',
@@ -13,14 +15,49 @@ export class MovieDetail extends PureComponent{
 
     constructor(props){
         super(props)
-        console.log(props.navigation.state.params.id)
+        this.state = {
+            loading: true,
+            movie: {}
+        }
     }
+
+    componentWillMount(){
+        this.fetchMovieDetails(this.props.navigation.state.params.id)
+    }
+
+    fetchMovieDetails = (movieID) => {
+        fetch(
+            `https://api.themoviedb.org/3/movie/${movieID}
+            ?api_key=60ff436f2769f4f8ddf76ac0cc28a39d`
+        )
+        .then(response => response.json())
+        .then(movie => this.setState({
+            movie,
+            loading: false
+        }))
+    }
+
+
     render = () => {
-        return (<View style={{
-            backgroundColor: 'black',
-            flex: 1
-        }}>
-            
-        </View>)
+        const { 
+            backdrop_path,
+            title
+        } = this.state.movie
+
+        return (
+            <View style={{
+                backgroundColor: 'black',
+                flex: 1,
+                flexDirection: 'column',
+                margin: 10
+            }}>
+                <Text style={{color: 'white'}}>{title}</Text>
+                <Image 
+                    source={{uri: `https://image.tmdb.org/t/p/w500/${backdrop_path}`}}
+                    style={{
+                        width: Dimensions.get('screen').width,
+                        height: 200
+                    }}/>
+            </View>)
     }
 }
