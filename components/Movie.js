1cd73732205a79368dd4a199d19e3f386af50087
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-27 01:50:18 -0200

    improvements: refactor movie and movielist components

--- components/Movie.js
+++ components/Movie.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Image, TouchableOpacity, Text, View } from 'react-native';
+import { Image, TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 
 export class Movie extends PureComponent {
 
@@ -12,15 +12,10 @@ export class Movie extends PureComponent {
         const { id, poster_path, title, vote_average,release_date } = movie;
         const year = new Date(release_date).getFullYear();
         return(
-            <View style={{width: 125, marginLeft: 2 }}>
-                <TouchableOpacity onPress={() => onPress(id)}>
+            <TouchableOpacity onPress={() => onPress(id)}>
+                <View style={styles.main}>
                     <Text 
-                        style={{
-                            color: 'white', 
-                            textAlign: 'center',
-                            textAlignVertical: 'center',
-                            minHeight: 40
-                        }}
+                        style={styles.title}
                         ellipsizeMode={'tail'}
                         numberOfLines={2}
                     >
@@ -28,18 +23,41 @@ export class Movie extends PureComponent {
                     </Text>
                     <Image 
                         source={{uri: `https://image.tmdb.org/t/p/w500/${poster_path}`}}
-                        style={{width: 125, height: 175}}
+                        style={styles.imageCover}
                     />
-                    <View style={{flex: 1, flexDirection: 'row', position: 'absolute', bottom: 0, backgroundColor: 'rgba(52, 52, 52, 0.7)' }}>
-                            <View style={{flex: 1, alignItems: 'center'}}>
+                    <View style={styles.score}>
+                            <View style={styles.scoreItem}>
                                 <Text style={{color: 'white'}}>{vote_average}</Text>
                             </View>
-                            <View style={{flex: 1, alignItems: 'center'}}>
+                            <View style={styles.scoreItem}>
                                 <Text style={{color: 'white'}}>{year}</Text>
                             </View>
                     </View>
-                </TouchableOpacity>
-            </View>
+                </View>
+            </TouchableOpacity>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    main: {
+        width: 125,
+        marginLeft: 2,
+        height: 200,
+    },
+    title: {
+        color: 'white', 
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        height: 40
+    },
+    imageCover: {width: 125, height: 175},
+    score: {
+        flex: 1,
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor: 'rgba(52, 52, 52, 0.7)'
+    },
+    scoreItem: {flex: 1, alignItems: 'center'}
+})
